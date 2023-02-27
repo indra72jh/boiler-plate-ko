1b35392f071c476cc9e0d95b3e04d15cacebd307
Indra <indra72@gmail.com>
2023-02-27 21:43:13 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true })); //application/x-www/form-urlencode
+app.use(bodyParser.json()); //application/json으로 된것을 가져올수 있게
 
 const mogoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017")
+  .connect(config.mogoURI)
   .then(() => console.log("MongoDB connected.."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! to nojs");
+});
+
+app.post("/register", (req, res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,32 +1,32 @@
-const mogoose = require('mongoose');
+const mogoose = require("mongoose");
 
 const userSchema = mogoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        trim: true,
-        unique: 1
-    },
-    password: {
-        type: String,
-        maxlength: 50
-    },
-    role: {
-        type: Number,
-        defult: 0
-    },
-    image: String,
-    token: {
-        type: String
-    }
-    tokenExp: {
-        type: Number
-    }
-})
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    defult: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
 
-const User = mogoose.model('User', userSchema);
+const User = mogoose.model("User", userSchema);
 
-module.exports = {User}
+module.exports = { User };
